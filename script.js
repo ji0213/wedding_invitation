21b347092213b3031d0886bc6188fb970c301b09
ji0213 <ji021314@gmail.com>
2026-09-16 17:17:24 +0900

    37

--- script.js
+++ script.js
@@ -195,7 +195,7 @@ loadGuestbook();
           });
 
           const infowindow = new kakao.maps.InfoWindow({
-            content: `<div style="padding:8px 10px;
+            content: `<div style="padding:6px 8px;
             font-size:14px;
             font-weight:700;
             color:#4a4137;
