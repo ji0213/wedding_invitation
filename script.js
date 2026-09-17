90fab1f6537481a206bcdf16ed5f3004d4f4b75f
ji0213 <ji021314@gmail.com>
2026-09-17 16:50:56 +0900

    52

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 (function(){
-  var text = '박정희 ♡ 최지우 결혼합니다.'
+  var text = '박정희♡최지우 결혼합니다.'
   var el = document.getElementById('openingText');
   document.body.style.overflow='hidden';
   [...text].forEach(function(ch,i){
     var span = document.createElement('span');
     span.className ='ch';
-    span.textContent = ch ===' '?'\u00A0' : ch;
+    span.textContent = ch === ' ' ? '\u00A0' : ch;
     el.appendChild(span);
     setTimeout(function(){
       span.classList.add('in');
-    },300+i*120);
+    }, 300 + i * 120);
   });
-  var total = 300 + text.length*120+900;
+  var total = 300 + text.length * 120 + 900;
   setTimeout(function(){
     document.getElementById('openingOverlay').classList.add('hide');
     document.body.style.overflow ='';
-  },total);
+  }, total);
 })();
 
 (function(){
