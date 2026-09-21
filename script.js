7a52bacfc2fa6048caf57018c7bb5a700a19c633
ji0213 <ji021314@gmail.com>
2026-09-21 10:29:45 +0900

    64

--- script.js
+++ script.js
@@ -419,7 +419,7 @@ function shareKakao(){
     content: {
       title: '박정희♡최지우 결혼합니다',
       description: '2027.03.13 토요일 엔팰리스웨딩컨벤션',
-      imageUrl: 'https://ji0213.github.io/wedding_invitation/asd.png',
+      imageUrl: 'https://ji0213.github.io/wedding_invitation/image/asd.png',
       link: {
         mobileWebUrl:window.location.href,
         webUrl: window.location.href
